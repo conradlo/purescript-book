5c66593c555f4008e641d4b60655737a136b54de
Conrad Lo <email@conradlo.com>
2018-10-24 17:23:03 +0800

    [chapter2] migrate to purescript 0.12 & use psc-package

--- exercises/chapter2/src/Main.purs
+++ exercises/chapter2/src/Main.purs
@@ -1,7 +1,7 @@
 module Main where
 
 import Prelude ((+), (*))
-import Control.Monad.Eff.Console (logShow)
+import Effect.Console (logShow)
 import Math (sqrt)
 
 diagonal :: Number -> Number -> Number
