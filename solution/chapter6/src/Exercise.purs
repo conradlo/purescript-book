7a4de1cc973ec734f20b0ad9e80e6fab5db07334
Conrad Lo <email@conradlo.com>
2018-10-27 16:34:30 +0800

    ex 6.11

--- solution/chapter6/src/Exercise.purs
+++ solution/chapter6/src/Exercise.purs
@@ -3,7 +3,8 @@ module Exercise where
 import Prelude
 
 import Data.Array ((:))
-import Data.Foldable (class Foldable, foldr, foldl)
+import Data.Foldable (class Foldable, foldr, foldl, maximum)
+import Data.Maybe (fromJust)
 
 -- 6.4 Ex 1
 -- The following newtype represents a complex number:
@@ -83,3 +84,137 @@ instance foldableNonEmpty :: Foldable (NonEmpty) where
   foldl func init_b (NonEmpty el arr) = foldl func (func init_b el) arr
   foldMap :: forall a m. Monoid m => (a -> m) -> NonEmpty a -> m
   foldMap func (NonEmpty a arr) = foldl append mempty (map func (NonEmpty a arr))
+
+-- 6.7 Ex 6
+{-
+  Given a type constructor `f` which defines an ordered container
+  (and so has a `Foldable` instance), we can create a new container type
+  which includes an extra element at the front:
+-}
+
+data OneMore f a = OneMore a (f a)
+
+{-
+  The container `OneMore f` is also has an ordering,
+  where the new element comes before any element of `f`.
+  Write a `Foldable` instance for `OneMore f`: 
+-}
+
+-- to support `foldMap`, `f` should be a Functor as well (?)
+instance mapOneMore :: Functor f => Functor (OneMore f) where
+  map :: forall f a b. Functor f => (a -> b) -> OneMore f a -> OneMore f b
+  map func (OneMore a fa) = OneMore (func a) (map func fa)
+
+instance foldableOneMore :: (Functor f, Foldable f) => Foldable (OneMore f) where
+  foldr :: forall f a b. Foldable f => (a -> b -> b) -> b -> OneMore f a -> b
+  foldr func init_b (OneMore a fa) = foldr func (func a init_b) fa
+  foldl :: forall f a b. Foldable f => (b -> a -> b) -> b -> OneMore f a -> b
+  foldl func init_b (OneMore a fa) = foldl func (func init_b a) fa
+  foldMap :: forall f a m. Functor f => Foldable f => Monoid m => (a -> m) -> OneMore f a -> m
+  foldMap func (OneMore a fa) = foldl append mempty (map func (OneMore a fa))
+
+
+-- 6.11 Ex 1
+{- 
+  Define a partial function which finds the maximum of a non-empty array of integers.
+  Your function should have type `Partial => Array Int -> Int`.
+  Test out your function in PSCi using `unsafePartial`. Hint: Use the `maximum` function from `Data.Foldable`.
+-}
+maxInt :: Partial => Array Int -> Int
+maxInt xs = fromJust $ maximum xs
+
+-- 6.11 Ex 2
+
+class Monoid m <= Action m a where
+  act :: m -> a -> a
+
+newtype Multiply = Multiply Int
+
+instance semigroupMultiply :: Semigroup Multiply where
+  append (Multiply n) (Multiply m) = Multiply (n * m)
+
+instance monoidMultiply :: Monoid Multiply where
+  mempty = Multiply 1
+
+instance repeatAction :: Action Multiply String where
+  act :: Multiply -> String -> String
+  act (Multiply n) str | n < 1 = ""
+                       | otherwise = str <> act (Multiply (n - 1)) str
+
+{-
+  Action laws:
+  1. act mempty a = a
+  2. act (m1 <> m2) a = act m1 (act m2 a)
+
+  1st law holds
+  mempty = Multiply 1:
+  act (Multiply 1) "string" == "string"
+
+  if n, m > 0
+  set m1 = Multiply 2, m2 = Multiply 3:
+  act (Multiply 2 <> Multiply 3) "<string>"
+    == act (Multiply 2) (act (Multiply 3) "<string>")
+    == "<string><string><string><string><string><string>"
+
+  what if '0' or negative Int in `Multiply`?
+
+  if n == 0, forall m
+  act (Multiply 0 <> Multiply m) "<string>"
+    == act (Multiply 0) "<string>"
+    == ""
+  act (Multiply 0) (act (Multiply m) "<string>") == ""
+  act (Multiply m) (act (Multiply 0) "<string>") == ""
+
+  if n, m < 0
+  act (Multiply -2 <> Multiply -3) "<string>" == act (Multiply 6) "<string>"
+  /=
+  act (Multiply -2) (act (Multiply -3) "<string>") == ""
+
+  Does this instance satisfy the laws listed above? Ans: Yes iff n,m not both negative forall (Multiply n) (Multiply m)
+-}
+
+-- 6.11 Ex 3
+{-
+ Write an instance `Action m a => Action m (Array a)`
+ where the action on arrays is defined by acting on each array element independently.
+-}
+instance actOnArray :: Action m a => Action m (Array a) where
+  act :: m -> Array a -> Array a
+  act m = map (act m)
+
+-- 6.11 Ex 4
+{-
+  Given the following newtype, write an instance for `Action m (Self m)`
+  where the monoid `m` acts on itself using `append`:
+-}
+newtype Self m = Self m
+instance actOnSelf :: Monoid m => Action m (Self m) where
+  act :: m -> Self m -> Self m
+  act m (Self n) = Self (append m n)
+
+-- 6.11 Ex 5
+{-
+  Should the arguments of the multi-parameter type class `Action`
+  be related by some functional dependency? Why or why not?
+-}
+
+{-
+  Ans:
+    No. given a `m` of type `Monoid` doesn't automactically indicated the type of `a`
+    In fact, the above example shows given the same type (Monoid m), the `act` function
+    can return (and accept) different types of `a`
+      e.g.
+        given (Monoid m) -> Array a -> Array a, here a is an `Array a`
+        given (Monoid m) -> Self m -> Self m, here a is an `Self m`
+  
+  moreover, if added functional dependency e.g.
+  ```
+  class Monoid m <= Action m a | m -> a where
+    act :: m -> a -> a
+  ```
+  compiler shows "Overlapping type class instances" errors on any 2 of the following instances
+    Exercise.repeatAction
+    Exercise.actOnArray
+    Exercise.actOnSelf
+  
+-}
