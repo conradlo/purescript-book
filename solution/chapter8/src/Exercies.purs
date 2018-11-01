b3e6a609a0fd4bd4c58d6253e5fa62e8dd251fc9
Conrad Lo <email@conradlo.com>
2018-11-01 15:15:00 +0800

    ex 8.7 Q4

--- solution/chapter8/src/Exercies.purs
+++ solution/chapter8/src/Exercies.purs
@@ -33,3 +33,44 @@ sums arr = (sort <<< nub <<< foldM (\x y -> [x, y, x + y]) 0) arr
  apply :: forall a b m. Monad m => m (a -> b) -> m a -> m b
  which is the same as `ap`
 -}
+
+-- 8.7 Ex 4
+{-
+  Verify that the monad laws hold for the `Monad` instance for the `Maybe` type,
+  as defined in the `purescript-maybe` package.
+-}
+
+{-
+  `Monad` type class definition
+  `class (Applicative m, Bind m) <= Monad m`
+
+  laws for monad:
+  1. Left Identity: `pure x >>= f = f x`
+  2. Right Identity: `x >>= pure = x`
+  3. Applicative Superclass: `apply = ap`
+
+  1. check Left Identity
+
+  ```
+    instance bindMaybe :: Bind Maybe where
+      bind (Just x) k = k x
+      bind Nothing  _ = Nothing
+  ```
+  ```
+    instance applicativeMaybe :: Applicative Maybe where
+      pure = Just
+  ```
+  LHS: `pure x >>= f`
+  equal `bind (pure x) f`
+  equal `bind (Just x) f`
+  equal `f x` (from definition)
+  equal == RHS
+
+  2. check Right Identity (actually don't know the ans)
+  `x >>= pure = x`
+  equal bind x pure
+  for some primitive value x, pure x == x
+
+  3. checked in ex. 3
+
+-}
